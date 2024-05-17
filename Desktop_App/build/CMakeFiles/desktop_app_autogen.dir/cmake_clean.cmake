434b4ae137cf0328892ff5215fdefb3b8c4a1c8a
Adrian Hałys <34137984+Bl0tniaQus@users.noreply.github.com>
2024-05-17 16:18:28 +0200

    Delete Desktop_App/build directory

--- Desktop_App/build/CMakeFiles/desktop_app_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/desktop_app_autogen"
-  "desktop_app_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/desktop_app_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
